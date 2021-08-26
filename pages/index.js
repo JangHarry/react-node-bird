d0279fb04ed46330a577f8e2a15580285beb3d31
장인천 <incheon@partner.sbs.co.kr>
2021-08-26 16:34:35 +0900

    no message

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import AppLayout from '../components/AppLayout';
-import Head from 'next/head';
-import { useSelector } from 'react-redux';
+import React from "react";
+import AppLayout from "../components/AppLayout";
+import Head from "next/head";
+import { useSelector } from "react-redux";
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
 
 const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
 
-    const {isLoggedIn}  = useSelector((state) => state.user);
-    const {mainPosts}  = useSelector((state) => state.post);
-
-    return( 
-        <>            
-            <AppLayout>
-                {isLoggedIn && <PostForm/>}
-                {mainPosts.map((post, index) => <PostCard key ={post.id} post ={ post} />)}
-            </AppLayout>
-        </>
-    )
-}
-
+  return (
+    <>
+      <AppLayout>
+        {isLoggedIn && <PostForm />}
+        {mainPosts.map((post, index) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </AppLayout>
+    </>
+  );
+};
 
 export default Home;
